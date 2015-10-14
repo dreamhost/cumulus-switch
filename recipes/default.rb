2f34c81d6e4dcfa57e24809671117055e908b9bb
Ian Clark <ian@f85.net>
2015-10-13 22:51:29 -0700

    major overhaul

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,38 +8,33 @@
 #
 include_recipe 'cumulus'
 
+# Notifying the networking service to reload doesn't work
+# for some reason ('up to date'), so we're getting the
+# desired results by notifying this instead
+execute 'reload_networking' do
+  command 'service networking reload'
+  action :nothing
+end
+
 # Bridges
 node.dh_network.bridge.each do |bridge, data|
-  # Execute to notify later
-  execute "ifup_#{bridge}" do
-    command "ifup #{bridge}"
-    action :nothing
-  end
-
   cumulus_bridge bridge do
     ports data.ports if data['ports']
     ipv4 data.ipv4 if data['ipv4']
     ipv6 data.ipv6 if data['ipv6']
-    #notifies :run, "execute[ifup_br-cust]", :delayed
-    # This shit doesn't work with vagrant + virtualbox + cumulus vx
+    notifies :run, "execute[reload_networking]", :delayed
   end
 end
 
 # Interfaces
 node.dh_network.interface.each do |interface, data|
-  # Execute to notify later
-  execute "ifup_#{interface}" do
-    command "ifup #{interface}"
-    action :nothing
-  end
-
   cumulus_interface interface do
-    ipv4 [data.ipv4] if data['ipv4']
-    ipv4 [data.ipv6] if data['ipv6']
+    ipv4 data.ipv4 if data['ipv4']
+    ipv6 data.ipv6 if data['ipv6']
     speed data.speed if data['speed']
     mtu data.mtu if data['mtu']
-    #notifies :run, "execute[ifup_#{interface}]", :delayed
-    # This shit doesn't work with vagrant + virtualbox + cumulus vx
+    addr_method data.addr_method if data['addr_method']
+    notifies :run, "execute[reload_networking]", :delayed
   end
 end
 
@@ -50,17 +45,10 @@ node.dh_network.interface_range.each do |range_str, data|
   (range[1]..range[2]).each do |id|
     ifname = range_str.gsub(/\[\d+-\d+\]/, id)
 
-    # Execute to notify later
-    execute "ifup_#{ifname}" do
-      command "ifup #{ifname}"
-      action :nothing
-    end
-
     cumulus_interface ifname do
       speed data.speed if data['speed']
       mtu data.mtu if data['mtu']
-      #notifies :run, "execute[ifup_#{ifname}]", :delayed
-      # This shit doesn't work with vagrant + virtualbox + cumulus vx
+      notifies :run, "execute[reload_networking]", :delayed
     end
   end
 end
@@ -69,7 +57,7 @@ end
 cumulus_ports 'speeds' do
   speed_10g node.dh_network.ports['10g'] if node.dh_network.ports['10g']
   speed_40g node.dh_network.ports['40g'] if node.dh_network.ports['40g']
-  speed_40_div_4 node.dh_network.ports['40_div_4'] if node.dh_network.ports['40_div_4']
+  speed_40g_div_4 node.dh_network.ports['40g_div_4'] if node.dh_network.ports['40g_div_4']
   speed_4_by_10g node.dh_network.ports['4_by_10g'] if node.dh_network.ports['4_by_10g']
-  notifies :restart, "service[switchd]"
+  notifies :restart, "service[switchd]", :delayed
 end
