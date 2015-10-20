d3394e3dc54a001a0ca4627a345e6a2365203de4
Ian Clark <ian@f85.net>
2015-10-20 01:37:23 -0700

    tweaking metadata after rename

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name             'cumulus-application-example'
-maintainer       'Jordan Tardif'
-maintainer_email 'jordan@dreamhost.com'
+name             'cumulus-attributes'
+maintainer       'Ian Clark'
+maintainer_email 'ian.clark@dreamhost.com'
 license          'All rights reserved'
-description      'Installs/Configures Cumulus Linux'
+description      'Configures interfaces from attributes via the cumulus cookbook providers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 depends 'cumulus'
