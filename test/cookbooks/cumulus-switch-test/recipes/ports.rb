45b2e90edaabf6a657b248855f81d0267fff6e01
Ian Clark <ian@f85.net>
2017-08-04 11:56:00 -0700

    bumping version and fixing tests

--- test/cookbooks/cumulus-switch-test/recipes/ports.rb
+++ test/cookbooks/cumulus-switch-test/recipes/ports.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: cumulus-switch-test
 # Recipe:: ports
 #
-node.set['cumulus']['ports']['10g'] = %w( 'swp1' )
-node.set['cumulus']['ports']['40g'] = %w( 'swp2-3' )
-node.set['cumulus']['ports']['40g_div_4'] = %w( 'swp4', 'swp5' )
-node.set['cumulus']['ports']['4_by_10g'] = %w( 'swp6', 'swp7-8' )
+node.set['cumulus']['ports']['10g'] = %w( swp1 )
+node.set['cumulus']['ports']['40g'] = %w( swp2-3 )
+node.set['cumulus']['ports']['40g_div_4'] = %w( swp4 swp5 )
+node.set['cumulus']['ports']['4_by_10g'] = %w( swp6 swp7-8 )
